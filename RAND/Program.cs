1c2584cbdccaadc2547d7f6f41c1f8aad3d13c24
jackkimmins <jackkimmins@outlook.com>
2022-01-02 10:14:42 +0000

    Add project files.

---
+++ RAND/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenCvSharp;
+using System.Runtime.InteropServices;
+
+namespace RAND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RAND random = new RAND();
+            
+            while (true)
+            {
+                Console.WriteLine(random.Next(1, 100));
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RAND/RAND.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenCvSharp;
+using System.Runtime.InteropServices;
+
+namespace RAND
+{
+    class RAND
+    {
+        private VideoCapture capture = new VideoCapture(1);
+        private byte[] WebCamSeeder()
+        {
+            if (!capture.IsOpened())
+                throw new Exception("WebCam Capture Failed to Open.");
+
+            Mat frame = new Mat();
+
+            capture.Read(frame);
+
+            byte[] data = new byte[frame.Total() * frame.ElemSize()];
+            Marshal.Copy(frame.Data, data, 0, data.Length);
+
+            return data;
+        }
+
+        public int Next(int start, int end)
+        {
+            Random rng = new Random(BitConverter.ToInt32(WebCamSeeder(), 0));
+            return rng.Next(start, end);
+        }
+    }
+}
